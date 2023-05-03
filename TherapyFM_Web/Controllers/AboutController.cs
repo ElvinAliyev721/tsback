31683e81ecb4267accb2425626e3ddb1519c59a6
Elvin <aliyeve669@gmail.com>
2023-05-03 07:37:37 +0400

    last commit

--- TherapyFM_Web/Controllers/AboutController.cs
+++ TherapyFM_Web/Controllers/AboutController.cs
@@ -1,4 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TherapyFM_Web.Models;
+
+//public class User
+//{
+//    public int Id { get; set; }
+//    public string Name { get; set; }
+//    public string Email { get; set; }
+//}
+
+public class MyOtherObject
+{
+    public string Property1 { get; set; }
+    public int Property2 { get; set; }
+}
+
 
 namespace TherapyFM_Web.Controllers
 {
@@ -7,6 +24,20 @@ namespace TherapyFM_Web.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult MyAction()
+        {
+            var user = new User { id = 1, whatsapp = "+994708411308", email = "john.doe@example.com" };
+            var myOtherObject = new MyOtherObject { Property1 = "some value", Property2 = 123 };
+
+            // Ana JSON nesnesini oluşturun ve kullanıcı ve diğer nesneyi içine yerleştirin
+            var json = new JObject();
+            json.Add("user", JObject.FromObject(user));
+            json.Add("myOtherObject", JObject.FromObject(myOtherObject));
+
+            // JSON verisini döndürün
+            return Content(json.ToString(), "application/json");
         }
     }
 }
