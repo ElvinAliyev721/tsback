fb1a19e43e41b6defd6ba9d7aafc36f8de7c288e
Elvin <aliyeve669@gmail.com>
2023-05-09 23:37:33 +0400

    better logo uplode

--- TherapyFM_Web/Areas/Admin/Controllers/LogoController.cs
+++ TherapyFM_Web/Areas/Admin/Controllers/LogoController.cs
@@ -8,9 +8,7 @@ namespace TherapyFM_Web.Areas.Admin.Controllers
         private readonly IWebHostEnvironment _environment;
         public LogoController(IWebHostEnvironment environment)
         {
-
             _environment = environment;
-
         }
         public IActionResult Index()
         {
@@ -19,71 +17,37 @@ namespace TherapyFM_Web.Areas.Admin.Controllers
         [HttpPost]
         public async Task<IActionResult> UploadLogo(IFormFile file)
         {
-            string directoryPath = Path.Combine(_environment.WebRootPath, "images", "icons");
-            //string fileNameWithoutExtension = "favicon";
-            //string searchPattern = fileNameWithoutExtension + ".*"; // use * to match any extension
-
-            //string[] searchfilePaths = Directory.GetFiles(directoryPath, searchPattern);
-
-            //if (searchfilePaths.Length > 0)
-            //{
-            //    // file found
-            //    string oldImagefilePath = searchfilePaths[0]; // get the first matching file path
-            //    System.IO.File.Delete(oldImagefilePath);
-            //}
-            if (Directory.Exists(directoryPath))
-            {
-                // Get all files in the folder
-                string[] files = Directory.GetFiles(directoryPath);
-
-                // Delete each file
-                foreach (string f in files)
-                {
-                    System.IO.File.Delete(f);
-                }
-            }
-
             if (file != null && file.Length > 0)
             {
-                // Get the file name and extension
+                string directoryPath = Path.Combine(_environment.WebRootPath, "images", "icons");
+
+                if (Directory.Exists(directoryPath))
+                {
+                    string[] files = Directory.GetFiles(directoryPath);
+                    foreach (string f in files)
+                    {
+                        System.IO.File.Delete(f);
+                    }
+                }
                 var fileName = Path.GetFileNameWithoutExtension(file.FileName);
                 var extension = Path.GetExtension(file.FileName);
-
-                // Generate a unique file name
                 var uniqueFileName = $"{fileName}_{Guid.NewGuid()}{extension}";
-
-                // Save the file to the wwwroot/images folder
                 var filePath = Path.Combine(_environment.WebRootPath, "images", "icons", uniqueFileName);
-                
+
                 using (var stream = new FileStream(filePath, FileMode.Create))
                 {
                     await file.CopyToAsync(stream);
                 }
-
-
-                
-
-
                 if (System.IO.File.Exists(filePath))
                 {
-                    // Get the extension of the file
                     string fileExtension = Path.GetExtension(filePath);
 
-                    // Construct the new file name with the updated name but the same extension
-                    string newFilePath = Path.Combine(_environment.WebRootPath, "images", "icons", "mainLogo" + fileExtension);
-
-                    // Rename the file
+                    string newFilePath = Path.Combine(_environment.WebRootPath,
+                                             "images", "icons", "mainLogo" + fileExtension);
                     System.IO.File.Move(filePath, newFilePath);
-
-
-                    
-
                 }
-
-                return RedirectToAction("Index","Dashboard");
-                //datajsona elave et
+                return RedirectToAction("Index", "Dashboard");
             }
-
             return BadRequest("No file selected");
         }
     }
